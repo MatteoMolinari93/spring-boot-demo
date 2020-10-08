2e86ac1fda1dc9a3998a1e5434cf7b7ef564363f
Molinari <matteomolinari93@gmail.com>
2020-10-08 08:29:58 +0200

    Custom properties

--- src/main/java/com/molim/springboot/demo/myapp/rest/FunRestController.java
+++ src/main/java/com/molim/springboot/demo/myapp/rest/FunRestController.java
@@ -2,11 +2,15 @@ package com.molim.springboot.demo.myapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	@Value("${custom.name}")
+	private String customName;
 
 	@GetMapping("/")
 	public String helloWorld() {
@@ -16,6 +20,11 @@ public class FunRestController {
 	@GetMapping("/test-dev-tools")
 	public String testDevTools() {
 		return "Test dev tools. Time on Server is: " + LocalDateTime.now();
+	}
+	
+	@GetMapping("/test-custom-property")
+	public String tesCustomProperty() {
+		return customName;
 	}
 	
 }
